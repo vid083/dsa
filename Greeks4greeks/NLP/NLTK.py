a3139fc2ea74f6bf8ec27bf78deabb21dcf4b4be
ramagiriv <ramagirividya03@gmail.com>
2021-10-04 19:16:12 +0100

    project codes

---
+++ Greeks4greeks/NLP/NLTK.py
@@ -0,0 +1,29 @@
+from nltk import ne_chunk, pos_tag, word_tokenize
+from nltk.tree import Tree
+
+def get_continuous_chunks(text):
+    chunked = ne_chunk(pos_tag(word_tokenize(text)))
+    prev = None
+    continuous_chunk = []
+    current_chunk = []
+
+    for i in chunked:
+        if type(i) == Tree:
+            current_chunk.append(" ".join([token for token, pos in i.leaves()]))
+        elif current_chunk:
+            named_entity = " ".join(current_chunk)
+            if named_entity not in continuous_chunk:
+                continuous_chunk.append(named_entity)
+                current_chunk = []
+        else:
+            continue
+
+    if continuous_chunk:
+        named_entity = " ".join(current_chunk)
+        if named_entity not in continuous_chunk:
+            continuous_chunk.append(named_entity)
+            current_chunk = []
+    return continuous_chunk
+
+txt = "It serves as a major educational and health hub for the northern districts of Telangana.[7] It is a major business center and widely known for Granite and Agro-based industries. It is also called City of Granites"
+print (get_continuous_chunks(txt))
